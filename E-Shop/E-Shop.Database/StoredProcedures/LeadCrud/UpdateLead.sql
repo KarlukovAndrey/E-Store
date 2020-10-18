7aa1679fdde5268878d01d76f81efbbaf38935e8
AndreyKarlukov <karl911291@yandex.ru>
2020-10-18 19:24:24 +0300

    Added stored procedure UpdateLead

---
+++ E-Shop/E-Shop.Database/StoredProcedures/LeadCrud/UpdateLead.sql
@@ -0,0 +1,25 @@
+﻿CREATE PROCEDURE [dbo].[UpdateLead]
+  @Id BIGINT,
+  @FirstName NVARCHAR(50),
+  @LastName NVARCHAR(50),
+  @Birthday DATETIME2,
+  @Address NVARCHAR(40),
+  @CityId INT,
+  @Phone NVARCHAR(20),
+  @Email NVARCHAR(30),
+  @RoleId INT
+as
+  Begin
+	Update [dbo].[Lead] set
+	FirstName = @FirstName,
+	LastName = @LastName,
+	Birthday = @Birthday,
+	Address = @Address,
+	CityId = @CityId,
+	Phone = @Phone,
+	Email = @Email,
+	RoleId = @RoleId
+
+	where (Id = @Id)
+  END
+
