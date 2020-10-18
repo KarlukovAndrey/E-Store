253c9e89f6708be92fd96db0e565db810a01b548
AndreyKarlukov <karl911291@yandex.ru>
2020-10-18 18:17:18 +0300

    Added CreateLead

--- E-Shop/E-Shop.Database/StoredProcedures/LeadCrud/DeleatedLeadById.sql
+++ E-Shop/E-Shop.Database/StoredProcedures/LeadCrud/DeleatedLeadById.sql
@@ -1,13 +1,13 @@
 ﻿CREATE PROCEDURE [dbo].[DeleatedLeadById]
 	@Id BIGINT
 	as
-	begin
+	BEGIN
 	  update dbo.[Lead]
-	  set IsDeleted = 1
+	  Set IsDeleted = 1
 	  where(@Id = Id)
-	  select L.Id, L.FirstName, L.LastName, L.Email, L.Phone, L.RegistrationDate, L.Birthday, L.Address,C.Name, R.Id, R.Name
-	  from [dbo].[Lead] as L
+	  Select L.Id, L.FirstName, L.LastName, L.Email, L.Phone, L.RegistrationDate, L.Birthday, L.Address,C.Name, R.Id, R.Name
+	  From [dbo].[Lead] as L
 	  Join dbo.[City] as C on C.Id = L.CityId
 	  Join dbo.[Role] as R on R.Id = L.RoleId
 	  where(@Id = L.Id)
-	end
+	END
