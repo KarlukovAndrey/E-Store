a2a018e0e727fa8f38ab40c11400b641f6742f49
AndreyKarlukov <karl911291@yandex.ru>
2020-10-18 19:43:30 +0300

    Created CityDTO, LeadDTO, RoleDTO

---
+++ E-Shop/E-Shop.DAL/DTO/LeadDTO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace E_Shop.DAL.DTO
+{
+    public class LeadDTO
+    {
+        public long? Id {get; set;}
+        public string FirstName { get; set; } 
+        public string LastName { get; set; }
+        public DateTime RegistractionDate { get; set; }
+        public DateTime Birthday { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool isDeleted { get; set; }
+        public int CityId { get; set; }
+        public RoleDto Role { get; set; }
+        public CityDto City { get; set; }
+
+
+
+
+    }
+}
