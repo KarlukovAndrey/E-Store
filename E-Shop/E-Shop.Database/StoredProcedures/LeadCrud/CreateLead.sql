73a4895bf8a4b8ab111ad4eff283f06a3174adec
AndreyKarlukov <karl911291@yandex.ru>
2020-10-22 21:50:22 +0300

    Added Stored procedures
    CreateLead, DeleatedLeadById, UpdateLead

--- E-Shop/E-Shop.Database/StoredProcedures/LeadCrud/CreateLead.sql
+++ E-Shop/E-Shop.Database/StoredProcedures/LeadCrud/CreateLead.sql
@@ -6,10 +6,10 @@
 	@CityId int,
 	@Phone nvarchar(20),
 	@Email nvarchar(30),
-	@RoleId int
+	@Password nvarchar(60)
 	As
 	Begin
-		insert into [dbo].[Lead]
-		values (@FirstName, @LastName, @Birthday, @Address, @CityId, @Phone, @Email, @RoleId)
+		insert into [dbo].[Lead](FirstName, LastName, RegistrationDate, Birthday, Address, CityId, Phone, Email, Password)
+		values (@FirstName, @LastName,sysdatetime(), @Birthday, @Address, @CityId, @Phone, @Email,@Password)
 		select SCOPE_IDENTITY()
 	END
