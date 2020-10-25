297f8b5e8b0415b36f0c261e1c76712ca30f3ffc
AndreyKarlukov <karl911291@yandex.ru>
2020-10-25 18:08:59 +0300

    Added Dependency Injection

--- E-Shop/E-Shop.API/Startup.cs
+++ E-Shop/E-Shop.API/Startup.cs
@@ -1,33 +1,37 @@
+using Autofac;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Threading.Tasks;
+using E_Shop.API.Configuration;
+using E_Shop.Core.Settings;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using System.Reflection;
 
 namespace E_Shop.API
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration, IWebHostEnvironment webHostEnvironment)
         {
-            Configuration = configuration;
+            var builder = new ConfigurationBuilder();
+            builder.AddJsonFile("appsettings.json");
+            Configuration = builder.Build();
         }
 
-        public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureContainer(ContainerBuilder builder)
+        {
+            builder.RegisterModule<AutofacConfig>();
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.Configure<DBSettings>(Configuration);
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1",
@@ -42,7 +46,6 @@ namespace E_Shop.API
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
---
+++ E-Shop/E-Shop.Data/Repositories/DataWrapper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace E_Shop.Data.Repositories
+{
+    public class DataWrapper<T>
+    {
+        public T Data { get; set; }
+        public bool IsOk => ErrorMessage == null;
+        public string ErrorMessage { get; set; }
+    }
+}
