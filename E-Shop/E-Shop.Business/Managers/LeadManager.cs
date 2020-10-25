8ef19337d937a1da02a6f29dca179ff1e3c05e8f
AndreyKarlukov <karl911291@yandex.ru>
2020-10-25 18:39:14 +0300

    Mapping
    Added class MappingProfile

---
+++ E-Shop/E-Shop.Business/Managers/LeadManager.cs
@@ -0,0 +1,24 @@
+﻿using E_Shop.Business.Models.Output;
+using E_Shop.Data;
+using E_Shop.Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace E_Shop.Business.Managers
+{
+    public class LeadManager : ILeadManager
+    {
+        private ILeadRepository _leadRepository;
+        public LeadManager(ILeadRepository leadRepository) 
+        {
+            _leadRepository = leadRepository;
+        }
+        public DataWrapper<LeadOutputModel> DeleteLead(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+    }
+}
--- E-Shop/E-Shop.Data/DataWrapper.cs
+++ E-Shop/E-Shop.Data/DataWrapper.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace E_Shop.Data.Repositories
+namespace E_Shop.Data
 {
     public class DataWrapper<T>
     {
